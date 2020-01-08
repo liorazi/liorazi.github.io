08785ef44a44b8a676cf8eaa88ab7ad06ce135c2
Lior Azi <lior.azi@gmail.com>
2020-01-08 02:30:36 +0200

    update

--- download.py
+++ download.py
@@ -28,8 +28,8 @@ def check_for_new_file():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     prefs = { "download.default_directory": file_directory }
     chrome_options.add_experimental_option('prefs', prefs)
