9059e4fdf95dbeb541587bcf2739162a9b865b26
Lior Azi <lior.azi@gmail.com>
2020-01-11 21:36:06 +0200

    update

--- json_to_csv.py
+++ json_to_csv.py
@@ -23,4 +23,5 @@ df['SUG TAV'] = df['SUG TAV'].apply('{0:0>2}'.format)
 
 print("Saving JSON to CSV File")
 df.to_csv('tag.csv', sep='|', index=None)
-rc = subprocess.call("release.sh",shell=True)
+chmod +x release.sh
+rc = subprocess.call("./release.sh",shell=True)
