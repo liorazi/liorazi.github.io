0f0216dab89ae5787be28c3a1c977a470945382c
Lior Azi <lior.azi@gmail.com>
2020-01-11 22:03:17 +0200

    update

--- release.sh
+++ release.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 echo "About to push new tag.csv to github..."
+echo "Deleting old cache"
+rm -rf repo
 echo "Checking out repo..."
 mkdir repo
 cd repo
 echo "Cloning repo..."
 git clone https://github.com/liorazi/liorazi.github.io.git
-cp ../tag.csv .
+cd liorazi.github.io
+cp ../../tag.csv .
 git add tag.csv
 git commit -m "Heroku: Update tag.csv"
 echo "Pushing new csv to repo..."
